672c94260d476d4c687cb2bd0faf1e1c04fccae9
seooki <navproyuhs@naver.com>
2024-01-15 17:59:03 +0900

    update 01-15

--- bin/Source/MainView.js
+++ bin/Source/MainView.js
@@ -130,8 +130,8 @@ const getDate = () => {
 	const date = new Date();
 	const month = String(date.getMonth() + 1).padStart(2, '0');
 	const day = String(date.getDate()).padStart(2, '0');
-	const hour = date.getHours();
-	const min = date.getMinutes();
+	const hour = String(date.getHours()).padStart(2, '0');
+	const min = String(date.getMinutes()).padStart(2, '0');
 	
 	const result = `${month}.${day} ${hour}:${min}`;
 	
@@ -140,7 +140,7 @@ const getDate = () => {
 
 window.addEventListener('load', () => {
 	getDate();
-})
+});
 
 MainView.prototype.onALabel1Click = function(comp, info, e)
 {
@@ -157,4 +157,176 @@ MainView.prototype.onAView12Actionenter = function(comp, info, e)
 {
 	const view = document.querySelector('.hover_View');
 	view.style.display = 'block';
-}
+};
+
+MainView.prototype.onAView13Click = function(comp, info, e)
+{
+	
+	const nav = document.querySelector(".toggle_nav");
+	
+	nav.style.display = "none";
+	
+	
+
+};
+
+MainView.prototype.onAView14Click = function(comp, info, e)
+{
+		
+	const nav = document.querySelector(".toggle_nav");
+	
+	nav.style.display = "block";
+};
+
+let pageNum = 1;
+
+MainView.prototype.onAView15Click = function(comp, info, e)
+{
+
+	console.log(pageNum);
+	if(pageNum < 1) {
+		pageNum = 1;
+	} else {
+		pageNum--;
+	}
+	
+	pageView();
+};
+
+MainView.prototype.onAView16Click = function(comp, info, e)
+{
+	
+	console.log(pageNum);
+	if(pageNum > 4 ) {
+		pageNum = 4;
+	} else {
+		pageNum++;
+	}
+	
+	pageView();
+};
+
+const pageView = () => {
+
+	const page01 = document.querySelector(".news_01");
+	const page02 = document.querySelector(".news_02");
+	const page03 = document.querySelector(".news_03");
+	const page04 = document.querySelector(".news_04");
+
+	if(pageNum == 1) {
+		page01.style.display = "block";
+		page02.style.display = "none";
+		page03.style.display = "none";
+		page04.style.display = "none";
+	} else if (pageNum ==2) {		
+		page01.style.display = "none";
+		page02.style.display = "block";
+		page03.style.display = "none";
+		page04.style.display = "none";
+	} else if (pageNum == 3) {
+		page01.style.display = "none";
+		page02.style.display = "none";
+		page03.style.display = "block";
+		page04.style.display = "none";
+	} else if (pageNum == 4) {		
+		page01.style.display = "none";
+		page02.style.display = "none";
+		page03.style.display = "none";
+		page04.style.display = "block";
+	}
+};
+
+const subPage = document.querySelector(".third_sub_page");
+const carPage = document.querySelector(".third_car_page");
+
+
+//sub_page
+MainView.prototype.onALabel4Click = function(comp, info, e)
+{
+	subPage.style.display = 'block';
+	carPage.style.display = 'none';
+};
+
+//car_page
+MainView.prototype.onALabel5Click = function(comp, info, e)
+{
+	carPage.style.display = 'block';
+	subPage.style.display = 'none';
+};
+
+
+
+let num;
+
+const newsPage = document.querySelector(".first_news_page");
+const setPage = document.querySelector(".first_set_page");
+
+//뉴스스탠드
+MainView.prototype.onALabel6Click = function(comp, info, e)
+{
+	num = 1;
+	onChangePage();
+};
+//언론사편집
+MainView.prototype.onALabel7Click = function(comp, info, e)
+{
+	num = 2;
+	onChangePage();
+};
+
+const onChangePage = () => {
+	switch(num) {
+		case 1: 
+			newsPage.style.display = 'block';
+			setPage.style.display = 'none';
+			break;
+		case 2: 
+			newsPage.style.display = 'none';
+			setPage.style.display = 'block';
+			break;
+	}
+};
+
+const thirdSlide = document.querySelectorAll(".third_slideContainer > div");
+const thirdLeftBtn = document.querySelector(".third_left_btn");
+const thirdRightBtn = document.querySelector(".third_right_btn");
+
+const leftValue = 790;
+let moveValue = 0;
+let count = 0;
+//오른쪽버튼
+
+MainView.prototype.onAView18Click = function(comp, info, e)
+{
+	if(count >= 2) {
+		count = 2;
+		thirdRightBtn.style.display = 'none'
+	} else {
+		count++;
+		console.log(count)
+		moveValue = leftValue*count;
+	}
+	
+	for(let i=0; i<thirdSlide.length; i++) {
+		thirdSlide[i].style.left = `-${moveValue}px`;
+	}
+	
+	
+};
+
+MainView.prototype.onAView19Click = function(comp, info, e)
+{
+	if(count <= 0) {
+		count = 0;
+		
+	} else {
+		count--;
+		console.log(count)
+		moveValue = leftValue*count;
+			
+	}
+	
+	for(let i=0; i<thirdSlide.length; i++) {
+		thirdSlide[i].style.left = `-${moveValue}px`;
+	}
+};
